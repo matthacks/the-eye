bbc92abfc29ed2fbae139c532fa44e70498cf4b9
mattcorrente <mattscorrente@gmail.com>
2021-07-23 09:08:39 -0600

    update tests

--- eye/server/tests.py
+++ eye/server/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from .models import Application, Event, Session
+from datetime import datetime
 import json
 
 # THIS IS JUST THE BEGINNING; YOU CAN NEVER HAVE ENOUGH TESTS
@@ -63,14 +64,68 @@ class Creation(TestCase):
     def test_future_date(self):
         pass
 
-class Event(TestCase):
-    def test_event_queries(self):
-        pass
+class Queries(TestCase):
+    def setUp(self):
+        app = Application.objects.create(name="www.consumeraffairs.com", trusted=True)
+        session = Session.objects.create(identifier="1234564789-ABCD")
+        Event.objects.create(timestamp=datetime.now(), application=app, session=session, category = 'page interaction', name='pageview',
+            payload = { "host": "www.consumeraffairs.com", "path": "/", "element": "chat bubble" }, valid=True, invalid_reason = None
+        )
+        Event.objects.create(timestamp=datetime.now(), application=app, session=session, category = 'page interaction', name='btn click',
+            payload = { "host": "www.consumeraffairs.com", "path": "/", "element": "chat bubble" }, valid=False, invalid_reason = 'Time is from future...'
+        )
+        session = Session.objects.create(identifier="ABCD-1234564789")
+        Event.objects.create(timestamp=datetime.now(), application=app, session=session, category = 'page interaction', name='pageview',
+            payload = { "host": "www.consumeraffairs.com", "path": "/", "element": "chat bubble" }, valid=True, invalid_reason = None
+        )
 
-class Application(TestCase):
     def test_application_queries(self):
-        pass
+        c = Client()
+        response = c.get('/server/application')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 1)
 
-class Session(TestCase):
+    def test_event_queries(self):
+        c = Client()
+        response = c.get('/server/event')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 3)
+
+        response = c.get('/server/event/1234564789-ABCD')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 2)
+
+        response = c.get('/server/event/ABCD-1234564789')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 1)
+
+        response = c.get('/server/event/ABCD')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 0)
+
     def test_session_queries(self):
-        pass
+        c = Client()
+        response = c.get('/server/session')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 2)
+
+        response = c.get('/server/session/1234564789-ABCD')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 1)
+
+        response = c.get('/server/session/ABCD-1234564789')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 1)
+
+        response = c.get('/server/session/ABCD')
+        resp = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(resp), 0)
